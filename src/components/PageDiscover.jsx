31eefccce9901684e2a5aa9ca7ea6e2d853b52f7
Enzo Vandevelde <enzo@2a01cb0d8071fb4af78e941b949a9019.ipv6.abo.wanadoo.fr>
2023-10-20 09:59:49 +0200

    fix: card size

--- src/components/PageDiscover.jsx
+++ src/components/PageDiscover.jsx
@@ -34,6 +34,8 @@ const cardData = [
         description: 'Ancien joueur pro, j\'ai décidé de créer un site permettant de retrouver tout mes buts inscrits durant ma carrière, bon courage à vous il y en a beaucoup.',
         image: 'https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
     },
+    
+
 
 ];
 
@@ -44,53 +46,53 @@ export default function PageDiscover() {
         <>
             <SimpleGrid
                 spacing={1}
-                minChildWidth="500px"
+                minChildWidth="300px"  // Reduced size to fit 3 cards per row
                 maxHeight="fit-content"
                 overflowY="auto"
-                templateColumns='repeat(auto-fill, minmax(500px, 1fr))'
-            >            
-            {cardData.map((card, index) => (
-                <Card key={index} maxW='md' marginTop={'10px'} marginLeft={'auto'} marginRight={'auto'} marginBottom={'20px'}>
-                    <CardHeader>
-                        <Flex spacing='4'>
-                            <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                <Avatar name={card.name} src={card.avatar} />
-                                <Box>
-                                    <Heading size='sm'>{card.name}</Heading>
-                                    <Text>{card.role}</Text>
-                                </Box>
+                templateColumns='repeat(3, 1fr)'  // Set to repeat 3 times for 3 cards per row
+            >
+                {cardData.map((card, index) => (
+                    <Card key={index} maxW='xs' marginTop={'10px'} marginLeft={'auto'} marginRight={'auto'} marginBottom={'20px'}>
+                        <CardHeader>
+                            <Flex spacing='4'>
+                                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                                    <Avatar name={card.name} src={card.avatar} />
+                                    <Box>
+                                        <Heading size='sm'>{card.name}</Heading>
+                                        <Text>{card.role}</Text>
+                                    </Box>
+                                </Flex>
+                                <IconButton
+                                    variant='ghost'
+                                    colorScheme='gray'
+                                    aria-label='See menu'
+                                />
                             </Flex>
-                            <IconButton
-                                variant='ghost'
-                                colorScheme='gray'
-                                aria-label='See menu'
-                            />
-                        </Flex>
-                    </CardHeader>
-                    <CardBody>
-                        <Text>{card.description}</Text>
-                    </CardBody>
-                    <Image
-                        objectFit='cover'
-                        src={card.image}
-                        alt='Chakra UI'
-                    />
+                        </CardHeader>
+                        <CardBody>
+                            <Text>{card.description}</Text>
+                        </CardBody>
+                        <Image
+                            objectFit='cover'
+                            src={card.image}
+                            alt='Chakra UI'
+                        />
 
-                    <CardFooter
-                        justify='space-between'
-                        flexWrap='wrap'
-                        sx={{
-                            '& > button': {
-                                minW: '136px',
-                            },
-                        }}
-                    >
-                        <Button flex='1' variant='ghost' color={'black.400, white.200'} leftIcon={<BiHomeAlt />}>
-                            Site du client
-                        </Button>
-                    </CardFooter>
-                </Card>
-            ))}
+                        <CardFooter
+                            justify='space-between'
+                            flexWrap='wrap'
+                            sx={{
+                                '& > button': {
+                                    minW: '136px',
+                                },
+                            }}
+                        >
+                            <Button flex='1' variant='ghost' color={'black.400, white.200'} leftIcon={<BiHomeAlt />}>
+                                Découvrir le site
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))}
             </SimpleGrid>
         </>
     );
